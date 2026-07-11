f6400166a85ca22c2908f0733e242f3ccac7b456
NghiaDuong <nghia369ty@gmail.com>
2026-07-12 01:41:15 +0700

    update: replace turbo with webpack + polling for nextjs devcontainer as HMR does not work well with devcontainers

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
 };
+
+// Enable Webpack watch mode if USE_WEBPACK environment variable is set to true (for development in a devcontainer since HMR doesn't work well with devcontainers)
+if (process.env.USE_WEBPACK === "true")
+{
+  nextConfig.webpack = (config, context) => {
+    if (context.dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  };
+}
 
 export default nextConfig;
