385eea665d5464710bedd46c7b59569f4fa642c0
NghiaDuong <nghia369ty@gmail.com>
2026-07-18 11:07:52 +0700

    update: add background image for the portfolio site

--- nextjs/app/portfolio/layout.tsx
+++ nextjs/app/portfolio/layout.tsx
@@ -3,5 +3,10 @@ export default function PortfolioRootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="w-full h-full">{children}</div>;
+  return (
+    <div className="w-full *:min-h-screen flex flex-col relative">
+      <div className="w-full h-full fixed -z-10 opacity-50 bg-[url('/images/portfolio-bg.png')] bg-no-repeat bg-cover bg-center"></div>
+      <div className="w-full h-full">{children}</div>
+    </div>
+  );
 }
